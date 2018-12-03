99a9e1b51c56a088f0e089e3bec1a13b8ab896f4
Abrar124 <abrar.khurshid.124@gmail.com>
2018-12-03 18:37:57 +0500

    Javascript IOT practice

---
+++ index.js
@@ -0,0 +1,318 @@
+var name = "mark";
+
+console.log(name);
+
+name = "John"
+
+console.log(name);
+
+var nat;
+
+nat = "pk"
+/////////////////////////////////////////
+
+var weight = 150;
+
+console.log(weight + 25);
+
+// var originalNum = 23;
+// var newNum = originalNum + 7;
+
+// console.log("newNum",newNum);
+
+
+
+// var originalNum = 23;
+// var numToBeAdded = 7;
+// var newNum = originalNum + numToBeAdded;
+
+// console.log("newNum", newNum);
+
+
+// var originalNum = 90;
+// originalNum = originalNum + 10;
+
+// console.log("originalNum: ", originalNum)
+
+// var originalNum = "90";
+// originalNum = "90" + 10;
+
+// console.log("originalNum: ", originalNum)
+
+
+
+// var originalNumber = 23;
+// var newNumber = originalNumber + 7;
+
+// console.log("hello")
+
+
+
+// var 1stPresident = "Washington"; // NOT LEGAL
+
+
+// var myNameIsInzamam = "john"
+// var mynameisinzamam = "john"
+
+////////////////////////////////////////////////
+
+
+// var popularNumber = 4;
+
+// var popularNumber = 2 + 2;
+
+// console.log(4);
+
+
+// var popularNumber = 12 - 24;
+
+// var popularNumber = 3 * 12;
+
+// var num = 10;
+// var popularNumber = num + 200;
+
+// var num = 10;
+// var anotherNum = 1;
+// var popularNumber = num + anotherNum;
+
+// var whatsLeftOver = 13 % 5 ;
+// console.log(whatsLeftOver);
+
+
+/////////////////////////////////////////////////
+
+// var num = 5; // 6
+// num++;
+// console.log("num: ", num);
+
+
+// var num = 5;
+// num--;
+// console.log("num: ", num);
+
+
+// var num = 1;
+// var newNum = ++num;
+
+// console.log("new num ",newNum)
+
+
+
+// var num1 = 10; // 10
+// var num2 = 5; // 10 // 11
+// var num3 = 3; // 22 // 23
+// var num4 = 23; // 22 
+
+// num2 = num1++;
+// num1 = num2++;
+// num3 = --num4;
+// num4 = num3++;
+
+// console.log(num1); // 10 11 5
+// console.log(num2); //11
+// console.log(num3); // 23
+// console.log(num4); // 22
+
+
+
+
+// var num1 = 5;
+// var num2 = 8;
+
+// console.log(num1++); //5
+// console.log(++num2); // 9
+// console.log(num2--); // 9
+// console.log(--num1); //  5
+
+// console.log(num1);
+// console.log(num2);
+
+
+
+// var totalCost = 1 + 3 * 4;
+// console.log(totalCost);
+
+// totalCost = (1 + 3) * 4;
+// console.log(totalCost);
+
+// var resultOfComputation = (2 * 4) * 4 + 2;
+// console.log(resultOfComputation);
+
+// resultOfComputation = ((2 * 4) * 4) + 2;
+
+
+
+
+// var resultOfComputation = (2 * 4) * (4 + 2);
+// console.log(resultOfComputation);
+
+
+// console.log("Thanks for your input!");
+
+// var message = "Thanks for your input!";
+// console.log(message);
+
+
+
+// var userName = "Anjum bhai"
+// console.log("Thanks, " + userName + "!");
+
+
+
+
+// var userName = "Anjum bhai"
+// var message = "Thanks, ";
+// var banger = "!";
+// console.log(message + userName + banger);
+
+// console.log("2" + "2");
+
+
+// console.log("2 plus 2 equals " + (2 + 2) + 6) 
+
+
+// var spec = prompt("Your is your name?", "malik");
+
+
+
+// var MalikInput = prompt("please enter a number bw 1 to 10");
+// var AnjumInput = prompt("please enter a number bw 1 to 10");
+
+// if (MalikInput == AnjumInput) {
+//     console.log("Correct!");
+// };
+
+
+// var correctAnswer = "Vatican";
+// var x = "abc";
+
+// if (x === correctAnswer) {
+//     console.log("Correct!");
+//     console.log("pakistan");
+// }
+
+
+
+
+// if (fullName === "Mark" + " " + "Myers") {
+// if (fullName === firstName + " " + "Myers") {
+// if (fullName === firstName + " " + "Myers") {
+// if (fullName === firstName + " " + lastName) {
+// if (totalCost === 81.50 + 135) {
+// if (totalCost === materialsCost + 135) {
+// if (totalCost === materialsCost + laborCost) {
+// if (x + y === a - b) {
+
+
+
+
+// var yourTicketNumber = 1234;
+
+// if (yourTicketNumber !== 487208) {
+//     console.log("Better luck next time.");
+
+// }
+
+
+
+// console.log(1 > 0);
+// console.log(0 < 1);
+
+// console.log(1 >= 0);
+// console.log(1 >= 1);
+// console.log(0 <= 1);
+// console.log(1 <= 1);
+
+
+// var x = prompt("Where does the Pope live?");
+// if (x === "Vatican") {
+//     alert("Correct!");
+// }
+
+
+// var x = prompt("Where does the Pope live?");
+
+// if (x === "Vatican") {
+//     alert("Correct!");
+// }
+// if (x !== "Vatican") {
+//     alert("Wrong answer");
+// }
+
+
+// var x = "karachi"
+
+// if (x === "Vatican") {
+
+//     console.log("Correct!");
+// } 
+// else {
+//     console.log("Wrong answer");
+//     console.log("imp");
+// }
+
+
+
+// var correctAnswer = "Vatican";
+// if (x === correctAnswer) {
+//     alert("Correct!");
+// }
+// else {
+//     score--;
+//     userIQ = "problematic";
+//     alert("Incorrect");
+// }
+
+
+// var correctAnswer = "Vatican";
+
+// if (x === "ami to nhe dekh rahi hain") {
+//     topiPehenLo()
+// }
+// else if (temp < 20) {
+//     topiPehenLo()
+// }
+// else {
+//     alert("Incorrect");
+// }
+
+
+// var weight = 350;
+// var time = 5;
+
+// if (weight > 300 && time < 6) {
+//     console.log("give invite");
+// }
+// else {
+//     console.log("dont give invite");
+// }
+
+
+
+// if (weight > 300 && time < 6 && age > 17 && gender === "male") {
+//     alert("Come to our tryout!");
+// }
+// else {
+//     alert("Come to our cookout!");
+// }
+
+// var SAT = 30;
+// var GPA = 2.0;
+// var sport = "football";
+
+// if (SAT > 45 || GPA > 2.5 || sport === "football") {
+//     console.log("Welcome to Bubba State!");
+// }
+// else {
+//     console.log("Have you looked into appliance repair?");
+// }
+
+
+var age = 70;
+var res = "P.k."
+
+if ((age > 65 || age < 21) && res === "U.S.") {
+    console.log("correct")
+} else {
+    console.log("incorrect")
+}
